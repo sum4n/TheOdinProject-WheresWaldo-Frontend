681a79b5dd7ba60a75f45237064a074ff7edc61b
Suman Das <sum4ndas@gmail.com>
2025-09-08 21:35:37 +0530

    Add image and toggle DropDownMenu on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import waldoImg from "./assets/waldo.jpeg";
 import "./App.css";
+import DropDownMenu from "./components/DropDownMenu/DropDownMenu";
 
 function App() {
-  return <></>;
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  function clickImgHandler(e) {
+    console.log(e.currentTarget);
+    setShowDropDown((prev) => !prev);
+  }
+
+  return (
+    <>
+      <img
+        className="waldoImg"
+        src={waldoImg}
+        alt="crowd at beach"
+        onClick={clickImgHandler}
+      />
+      {!showDropDown ? null : <DropDownMenu />}
+    </>
+  );
 }
 
 export default App;
