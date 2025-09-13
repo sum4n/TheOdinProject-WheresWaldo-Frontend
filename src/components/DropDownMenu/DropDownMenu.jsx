9bd71500256ea7bb37fe1f867bd6186cd36fde99
Suman Das <sum4ndas@gmail.com>
2025-09-13 23:27:04 +0530

    Style dropDownMenu and add click handler on individual menu items

--- src/components/DropDownMenu/DropDownMenu.jsx
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -1,5 +1,28 @@
+import styles from "./DropDownMenu.module.css";
+
 function DropDownMenu({ style }) {
-  return <div style={style}>Hello </div>;
+  return (
+    <div style={style} className={styles.container}>
+      <p
+        className={styles.selection}
+        onClick={(e) => console.log(e.currentTarget)}
+      >
+        Waldo
+      </p>
+      <p
+        className={styles.selection}
+        onClick={(e) => console.log(e.currentTarget)}
+      >
+        Wizard
+      </p>
+      <p
+        className={styles.selection}
+        onClick={(e) => console.log(e.currentTarget)}
+      >
+        Wanda
+      </p>
+    </div>
+  );
 }
 
 export default DropDownMenu;
