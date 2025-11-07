b3e5b19384d2aed953ec33e3b2f19073a90f5662
Suman Das <sum4ndas@gmail.com>
2025-11-07 17:42:15 +0530

    feat(component): create Rank component

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+function Rank() {
+  const [scores, setScores] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/score", {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setScores(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <p>Rank:</p>
+      {!scores && <p>Loading...</p>}
+      {scores && scores.length === 0 && <p>No ranking found...</p>}
+      {scores && scores.length > 0 && (
+        <ol>
+          {scores.map((score) => {
+            return (
+              <li key={score.id}>
+                {score.username} - {score.time}
+              </li>
+            );
+          })}
+        </ol>
+      )}
+      <Link to={"/"}>Start new game</Link>
+    </>
+  );
+}
+
+export default Rank;
