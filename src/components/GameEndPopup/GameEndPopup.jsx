7c98280f9dd98296b491186c351ccef1b3e0acbf
Suman Das <sum4ndas@gmail.com>
2025-11-15 13:19:58 +0530

    feat(component): display time taken and rank on game end popup, remove props from the component.

--- src/components/GameEndPopup/GameEndPopup.jsx
+++ src/components/GameEndPopup/GameEndPopup.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./GameEndPopup.module.css";
 import { useNavigate } from "react-router";
 
-function GameEndPopup({ timeTaken }) {
+function GameEndPopup() {
   const [username, setUserName] = useState("");
   const [errors, setErrors] = useState();
+  const [resultData, setResultData] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/score", {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setResultData(data);
+      });
+  }, []);
 
   let navigate = useNavigate();
 
@@ -37,7 +49,8 @@ function GameEndPopup({ timeTaken }) {
     <div className={styles.container}>
       <p className={styles.gzText}>Congratulations!</p>
       <p className={styles.text}>You've found all characters!!</p>
-      <p className={styles.text}>Time taken: {timeTaken}</p>
+      <p className={styles.text}>Time taken: {resultData.timeElapsed}</p>
+      <p className={styles.text}>Rank: {resultData.rank}</p>
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">Enter name to check ranking:</label>
         <br />
