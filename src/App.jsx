b3abaee4841f642eb9977742df80784a7a3bc00c
Suman Das <sum4ndas@gmail.com>
2025-12-25 14:16:12 +0530

    fix(component): handle fetch errors and add error and loading state

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,26 @@ import { Outlet } from "react-router";
 
 function App() {
   const [boardList, setBoardList] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:3000/api/gameboards", { credentials: "include" })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
       .then((data) => {
         // console.log(data);
         setBoardList(data);
-      });
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
 
-  return <Outlet context={{ boardList }} />;
+  return <Outlet context={{ boardList, error, loading }} />;
 }
 
 export default App;
